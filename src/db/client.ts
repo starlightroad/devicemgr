c9ef64e87d1b4172752ec821658250738d24c1a6
starlightroad <ortizge92@gmail.com>
2026-03-25 22:35:33 -0400

    Connect Drizzle ORM to the db

---
+++ src/db/client.ts
@@ -0,0 +1,7 @@
+import { loadEnvConfig } from "@next/env";
+
+import { drizzle } from "drizzle-orm/node-postgres";
+
+loadEnvConfig(process.cwd());
+
+export const db = drizzle(process.env.DATABASE_URL!);
