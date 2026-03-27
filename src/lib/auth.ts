04961950e00c8d906d2c0421a4b51a90438411d3
starlightroad <ortizge92@gmail.com>
2026-03-26 22:30:44 -0400

    Configure database in auth instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,11 @@
 import { betterAuth } from "better-auth";
 
-export const auth = betterAuth({});
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "@/db/client";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+});
