88ee202cb08b8362fd07e922f4c644d504134006
starlightroad <ortizge92@gmail.com>
2026-03-26 23:58:48 -0400

    Add schemas to the auth instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,12 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db/client";
 
+import * as schemas from "@/db/schemas";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: schemas,
   }),
   user: {
     modelName: "users",
