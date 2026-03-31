49d2ffd30f01e937327b0b2e4ac78a569b4ddfb4
starlightroad <ortizge92@gmail.com>
2026-03-30 23:00:50 -0400

    Add suppressHydrationWarning to html to prevent hydration mismatches

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${interSans.variable} h-full antialiased`}>
+    <html lang="en" className={`${interSans.variable} h-full antialiased`} suppressHydrationWarning>
       <body className="flex min-h-full flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="root">{children}</div>
