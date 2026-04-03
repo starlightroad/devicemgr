7d456c025f41d7b0b53424fc4b23d1ba1b4950c6
starlightroad <ortizge92@gmail.com>
2026-04-03 15:16:30 -0400

    Replace theme button

--- src/components/theme-button.tsx
+++ src/components/theme-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@base-ui/react";
+import { Button } from "@heroui/react";
 
 import { useTheme } from "@wrksz/themes/client";
 
@@ -12,10 +12,10 @@ export default function ThemeButton() {
   return (
     <Button
       type="button"
-      title="Toggle theme"
-      aria-label="Toggle theme"
+      variant="ghost"
+      size="sm"
+      isIconOnly
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg text-neutral-800 outline-0 select-none focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-neutral-400"
     >
       <SunIcon className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <MoonIcon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 dark:text-neutral-100" />
