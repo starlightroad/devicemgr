188db43b8e703573d1ad012cad1de388b7dfe114
starlightroad <ortizge92@gmail.com>
2026-04-03 23:05:13 -0400

    Add tooltip to theme button

--- src/components/theme-button.tsx
+++ src/components/theme-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@heroui/react";
+import { Button, Tooltip } from "@heroui/react";
 
 import { useTheme } from "@wrksz/themes/client";
 
@@ -10,15 +10,22 @@ export default function ThemeButton() {
   const { resolvedTheme, setTheme } = useTheme();
 
   return (
-    <Button
-      type="button"
-      variant="ghost"
-      size="sm"
-      isIconOnly
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-    >
-      <SunIcon className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <MoonIcon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 dark:text-neutral-100" />
-    </Button>
+    <Tooltip delay={0} closeDelay={0}>
+      <Button
+        type="button"
+        variant="ghost"
+        size="sm"
+        isIconOnly
+        aria-label="Toggle theme"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      >
+        <SunIcon className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <MoonIcon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 dark:text-neutral-100" />
+      </Button>
+      <Tooltip.Content offset={12} showArrow>
+        <Tooltip.Arrow />
+        <p>Toggle theme</p>
+      </Tooltip.Content>
+    </Tooltip>
   );
 }
