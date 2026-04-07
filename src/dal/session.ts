a39f3b80ddc626160e2a37fb87ba9ca902de12f2
starlightroad <ortizge92@gmail.com>
2026-04-06 21:40:21 -0400

    Redirect unauthorized users to the login page

--- src/dal/session.ts
+++ src/dal/session.ts
@@ -2,12 +2,18 @@
 
 import { headers } from "next/headers";
 
+import { redirect } from "next/navigation";
+
 import { auth } from "@/lib/auth";
 
 export const getSession = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
+  if (!session?.session.userId) {
+    redirect("/login");
+  }
 
   return {
     userId: session?.session.userId,
