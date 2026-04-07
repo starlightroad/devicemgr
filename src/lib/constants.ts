de303710aa0cb74a7fa73220423d0459f7754f54
starlightroad <ortizge92@gmail.com>
2026-04-06 20:07:44 -0400

    Build the repo and sign out buttons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = "DeviceMGR";
 
 export const APP_DESC = "A device management dashboard built with Next.js.";
+
+export const GITHUB_REPO = "https://github.com/starlightroad/devicemgr";
