4f133a0c81d1b6f4d091f56aa17e2ac590955237
starlightroad <ortizge92@gmail.com>
2026-04-07 20:00:44 -0400

    Build a hook to sign out users

---
+++ src/features/auth/hooks/use-sign-out.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { client } from "@/features/auth";
+
+export default function useSignOut() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      const { data, error } = await client.signOut();
+
+      if (!data?.success) {
+        throw error;
+      }
+
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+
+      // Inform the user of the error
+    }
+  };
+
+  return {
+    signOut: handleSignOut,
+  };
+}
