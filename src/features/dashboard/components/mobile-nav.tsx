6c62d24e3260baaeba08fb5b95db44c5cb81b8cd
starlightroad <ortizge92@gmail.com>
2026-04-07 21:40:20 -0400

    Set role of nav element to Navigation

--- src/features/dashboard/components/mobile-nav.tsx
+++ src/features/dashboard/components/mobile-nav.tsx
@@ -6,7 +6,7 @@ import { MobileNavMenu, UserProfile } from "@/features/dashboard";
 
 export default function MobileNav() {
   return (
-    <nav>
+    <nav role="Navigation">
       <Dropdown>
         <Tooltip delay={0} closeDelay={0}>
           <Button type="button" variant="ghost" size="sm" isIconOnly className="md:hidden">
