fa37ca57d4b792df9f7c18ddc391b216a92d185a
starlightroad <ortizge92@gmail.com>
2026-04-10 18:58:07 -0400

    Optimize devices seed

--- src/db/seeds/devices.ts
+++ src/db/seeds/devices.ts
@@ -14,51 +14,52 @@ import { deviceGroupsTable, devicesTable, deviceStatusesTable, deviceTypesTable,
 
 const devicesSeed = async (db: Database) => {
   try {
-    const insertedDevices = await Promise.all(
+    const guestUserEmail = usersJSON[0].email;
+    const data = await db.select({ id: usersTable.id }).from(usersTable).where(eq(usersTable.email, guestUserEmail));
+    const userId = data[0].id;
+
+    const getDeviceTypeData = async (columnName: string) =>
+      db
+        .select({ id: deviceTypesTable.id })
+        .from(deviceTypesTable)
+        .where(and(eq(deviceTypesTable.userId, userId), eq(deviceTypesTable.name, columnName)));
+
+    const getDeviceStatusData = async (columnName: string) =>
+      db
+        .select({ id: deviceStatusesTable.id })
+        .from(deviceStatusesTable)
+        .where(and(eq(deviceStatusesTable.userId, userId), eq(deviceStatusesTable.name, columnName)));
+
+    const getDeviceGroupData = async (columnName: string) =>
+      db
+        .select({ id: deviceGroupsTable.id })
+        .from(deviceGroupsTable)
+        .where(and(eq(deviceGroupsTable.userId, userId), eq(deviceGroupsTable.name, columnName)));
+
+    const devicesValues = await Promise.all(
       devicesJSON.map(async (device) => {
-        const guestUserEmail = usersJSON[0].email;
+        const [deviceTypeData, deviceStatusData, deviceGroupData] = await Promise.all([
+          getDeviceTypeData(device.device_type),
+          getDeviceStatusData(device.device_status),
+          getDeviceGroupData(device.device_group),
+        ]);
 
-        const data = await db
-          .select({ id: usersTable.id })
-          .from(usersTable)
-          .where(eq(usersTable.email, guestUserEmail));
-
-        const userId = data[0].id;
-
-        const deviceTypeData = await db
-          .select({ id: deviceTypesTable.id })
-          .from(deviceTypesTable)
-          .where(and(eq(deviceTypesTable.userId, userId), eq(deviceTypesTable.name, device.device_type)));
-
-        const deviceStatusData = await db
-          .select({ id: deviceStatusesTable.id })
-          .from(deviceStatusesTable)
-          .where(and(eq(deviceStatusesTable.userId, userId), eq(deviceStatusesTable.name, device.device_status)));
-
-        const deviceGroupData = await db
-          .select({ id: deviceGroupsTable.id })
-          .from(deviceGroupsTable)
-          .where(and(eq(deviceGroupsTable.userId, userId), eq(deviceGroupsTable.name, device.device_group)));
-
-        return db
-          .insert(devicesTable)
-          .values({
-            id: generateId(),
-            userId,
-            typeId: deviceTypeData[0].id,
-            statusId: deviceStatusData[0].id,
-            groupId: deviceGroupData[0].id,
-            name: device.name,
-            serialNumber: device.serial_number,
-            ipAddress: device.ip_address,
-          })
-          .returning({ id: devicesTable.id });
+        return {
+          id: generateId(),
+          userId,
+          typeId: deviceTypeData[0].id,
+          statusId: deviceStatusData[0].id,
+          groupId: deviceGroupData[0].id,
+          name: device.name,
+          serialNumber: device.serial_number,
+          ipAddress: device.ip_address,
+        };
       }),
     );
 
-    const devicesCount = insertedDevices.length;
+    const { rowCount } = await db.insert(devicesTable).values(devicesValues);
 
-    console.info(`✅ Seeded ${devicesCount} device${devicesCount === 1 ? "" : "s"}.`);
+    console.info(`✅ Seeded ${rowCount} device${rowCount === 1 ? "" : "s"}.`);
   } catch (error) {
     console.error("🔥 Failed to seed devices:", error);
   }
