858fae1a204abe037219df70be349041b29ca4cf
starlightroad <ortizge92@gmail.com>
2026-05-02 00:08:40 -0400

    Animate chevron icon

--- src/features/device/components/filter-popover.tsx
+++ src/features/device/components/filter-popover.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import { ChevronDownIcon } from "lucide-react";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
@@ -37,6 +39,10 @@ export default function FilterPopover({ label, items }: FilterPopoverProps) {
 
   const anchor = useComboboxAnchor();
 
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const iconStyles = `${popoverOpen ? "rotate-180" : "rotate-0"} transition-transform`;
+
   const value = getFilteredSearchParams(searchParams.getAll(label.toLowerCase()), items);
 
   const updateQueryString = (values: typeof value) => {
@@ -54,12 +60,12 @@ export default function FilterPopover({ label, items }: FilterPopoverProps) {
   };
 
   return (
-    <Popover>
+    <Popover onOpenChange={setPopoverOpen}>
       <PopoverTrigger
         render={
           <Button variant="outline" className="border-dashed">
             {label}
-            <ChevronDownIcon data-icon="inline-end" />
+            <ChevronDownIcon data-icon="inline-end" className={iconStyles} />
           </Button>
         }
       />
