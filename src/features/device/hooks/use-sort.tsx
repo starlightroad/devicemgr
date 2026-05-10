df2f486332175d7f52ef0328555ea04b6c1d22c3
starlightroad <ortizge92@gmail.com>
2026-05-09 23:53:41 -0400

    Remove useState hook from useSort hook

--- src/features/device/hooks/use-sort.tsx
+++ src/features/device/hooks/use-sort.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import { useState } from "react";
-
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import type { SortDirection } from "@/features/device/lib/definitions";
 
 import { SORT_BY_PARAM_ID, SORT_DIRECTION_PARAM_ID } from "@/features/device/lib/constants";
-
-type SortProps = {
-  name: string;
-  direction: SortDirection;
-};
-
-const createInitialState = (sortProps: SortProps) => {
-  const { name, direction } = sortProps;
-
-  if (!name || !direction) return null;
-
-  return { name, direction };
-};
 
 export default function useSort() {
   const pathname = usePathname();
@@ -28,17 +13,13 @@ export default function useSort() {
 
   const searchParams = useSearchParams();
 
-  const [sort, setSort] = useState<SortProps | null>(
-    createInitialState({
-      name: searchParams.get(SORT_BY_PARAM_ID) || "",
-      direction: (searchParams.get(SORT_DIRECTION_PARAM_ID) as SortDirection) || "asc",
-    }),
-  );
+  const sort = {
+    name: searchParams.get(SORT_BY_PARAM_ID) || "",
+    direction: (searchParams.get(SORT_DIRECTION_PARAM_ID) as SortDirection) || "asc",
+  };
 
   const handleSort = (value: string) => {
     const newSortByDirection = sort?.name === value.toLowerCase() && sort?.direction === "asc" ? "desc" : "asc";
-
-    setSort({ name: value.toLowerCase(), direction: newSortByDirection });
 
     const params = new URLSearchParams(searchParams);
 
