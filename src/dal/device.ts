71f453cd142ab1672d50800b71dc34b82c393c3c
starlightroad <ortizge92@gmail.com>
2026-05-11 17:41:59 -0400

    Create function that gets the total number of pages based on filters

--- src/dal/device.ts
+++ src/dal/device.ts
@@ -94,3 +94,29 @@ export const getDevices = async (options: Options): Promise<ActionResult<Device[
     };
   }
 };
+
+export const getDevicesPages = async (filters: Options["filters"]) => {
+  try {
+    const session = await getSession();
+
+    const data = await db
+      .select({ count: count() })
+      .from(devicesTable)
+      .innerJoin(deviceTypesTable, eq(devicesTable.typeId, deviceTypesTable.id))
+      .innerJoin(deviceStatusesTable, eq(devicesTable.statusId, deviceStatusesTable.id))
+      .innerJoin(deviceGroupsTable, eq(devicesTable.groupId, deviceGroupsTable.id))
+      .where(sql`${devicesTable.userId} = ${session.userId} ${getSqlWhereStatementByFilters(filters)}`);
+
+    const totalPages = Math.ceil(data[0].count / MAX_ROWS);
+
+    return {
+      data: totalPages,
+      error: null,
+    };
+  } catch {
+    return {
+      data: null,
+      error: "Failed to load data.",
+    };
+  }
+};
