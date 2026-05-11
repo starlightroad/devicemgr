f66f9ba415407d18ccac211fd454e67554efb7c4
starlightroad <ortizge92@gmail.com>
2026-05-11 17:44:43 -0400

    Build the pagination component

--- src/features/device/components/device-table.tsx
+++ src/features/device/components/device-table.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
-import { getDevices } from "@/dal/device";
+import { Suspense } from "react";
+
+import { getDevices, getDevicesPages } from "@/dal/device";
 
 import { cn } from "@/lib/utils";
 
@@ -15,6 +17,8 @@ import { getBadgeIconColorClassesByStatus } from "@/features/device/lib/utils";
 import { Badge } from "@/components/ui/badge";
 
 import { buttonVariants } from "@/components/ui/button";
+
+import Pagination from "@/features/device/components/pagination";
 
 import DeviceActions from "@/features/device/components/device-actions";
 
@@ -50,52 +54,59 @@ export default async function DeviceTable({ query, types, statuses, groups, sort
 
   const columns = getDeviceTableColumns();
 
+  const totalPagesPromise = getDevicesPages(options.filters);
+
   return (
-    <div className="overflow-hidden border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <DeviceTableColumns columns={columns} />
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {(error || !data?.length) && (
-            <NoResultsFoundRow columns={columns.length}>
-              <NoResultsFoundIcon />
-              <NoResultsFoundDescription>No devices to display.</NoResultsFoundDescription>
-            </NoResultsFoundRow>
-          )}
-          {data?.map((device) => {
-            return (
-              <TableRow key={device.id} className="[&>td]:px-4">
-                <TableCell>
-                  <Link href="#" className={cn(buttonVariants({ variant: "link", size: "sm" }), "px-0 underline")}>
-                    {device.name}
-                  </Link>
-                </TableCell>
-                <TableCell>{device.type}</TableCell>
-                <TableCell>
-                  <Badge variant="outline">
-                    <span
-                      className={cn(
-                        "size-1.5 rounded-full",
-                        getBadgeIconColorClassesByStatus(device.status.toLowerCase()),
-                      )}
-                    />
-                    {device.status}
-                  </Badge>
-                </TableCell>
-                <TableCell>{device.group}</TableCell>
-                <TableCell>{device.serialNumber}</TableCell>
-                <TableCell>{device.ipAddress ?? "-"}</TableCell>
-                <TableCell align="right">
-                  <DeviceActions device={device} types={[]} statuses={[]} groups={[]} />
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+    <div>
+      <div className="overflow-hidden border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <DeviceTableColumns columns={columns} />
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {(error || !data?.length) && (
+              <NoResultsFoundRow columns={columns.length}>
+                <NoResultsFoundIcon />
+                <NoResultsFoundDescription>No devices to display.</NoResultsFoundDescription>
+              </NoResultsFoundRow>
+            )}
+            {data?.map((device) => {
+              return (
+                <TableRow key={device.id} className="[&>td]:px-4">
+                  <TableCell>
+                    <Link href="#" className={cn(buttonVariants({ variant: "link", size: "sm" }), "px-0 underline")}>
+                      {device.name}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{device.type}</TableCell>
+                  <TableCell>
+                    <Badge variant="outline">
+                      <span
+                        className={cn(
+                          "size-1.5 rounded-full",
+                          getBadgeIconColorClassesByStatus(device.status.toLowerCase()),
+                        )}
+                      />
+                      {device.status}
+                    </Badge>
+                  </TableCell>
+                  <TableCell>{device.group}</TableCell>
+                  <TableCell>{device.serialNumber}</TableCell>
+                  <TableCell>{device.ipAddress ?? "-"}</TableCell>
+                  <TableCell align="right">
+                    <DeviceActions device={device} types={[]} statuses={[]} groups={[]} />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+      <Suspense fallback={<p className="text-sm">Loading pagination...</p>}>
+        <Pagination totalPagesPromise={totalPagesPromise} />
+      </Suspense>
     </div>
   );
 }
---
+++ src/features/device/components/pagination.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { use } from "react";
+
+import { ChevronLeftIcon, ChevronRightIcon, ChevronsLeftIcon, ChevronsRightIcon } from "lucide-react";
+
+import type { ActionResult } from "@/lib/definitions";
+
+import { PAGE_SIZES } from "@/features/device/lib/constants";
+
+import usePagination from "@/features/device/hooks/use-pagination";
+
+import { Button } from "@/components/ui/button";
+
+import { Field, FieldLabel } from "@/components/ui/field";
+
+import { ButtonGroup } from "@/components/ui/button-group";
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+type PaginationProps = {
+  totalPagesPromise: Promise<ActionResult<number>>;
+};
+
+export default function Pagination({ totalPagesPromise }: PaginationProps) {
+  const { data, error } = use(totalPagesPromise);
+
+  const totalPages = data ?? 0;
+
+  const { page, goToFirstPage, goToPreviousPage, goToNextPage, goToLastPage } = usePagination({ totalPages });
+
+  if (!data || error) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center justify-between gap-6 border-x border-transparent p-4">
+      <p className="text-sm">{`Page ${page} of ${totalPages}`}</p>
+      <div className="flex items-center gap-6">
+        <RowsPerPage />
+        <ButtonGroup>
+          <ButtonGroup>
+            <Button type="button" variant="outline" size="icon" disabled={page === 1} onClick={goToFirstPage}>
+              <ChevronsLeftIcon />
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button type="button" variant="outline" size="icon" disabled={page === 1} onClick={goToPreviousPage}>
+              <ChevronLeftIcon />
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button type="button" variant="outline" size="icon" disabled={page === totalPages} onClick={goToNextPage}>
+              <ChevronRightIcon />
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button type="button" variant="outline" size="icon" disabled={page === totalPages} onClick={goToLastPage}>
+              <ChevronsRightIcon />
+            </Button>
+          </ButtonGroup>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+}
+
+function RowsPerPage() {
+  return (
+    <Field orientation="horizontal" className="w-auto">
+      <FieldLabel htmlFor="rows-per-page">Rows per page</FieldLabel>
+      <Select defaultValue={PAGE_SIZES[0]}>
+        <SelectTrigger id="rows-per-page" className="w-16">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent align="start">
+          <SelectGroup>
+            {PAGE_SIZES.map((item) => {
+              const key = `rows-per-page-${item}`;
+
+              return (
+                <SelectItem key={key} value={item}>
+                  {item}
+                </SelectItem>
+              );
+            })}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </Field>
+  );
+}
