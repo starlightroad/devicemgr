6fbbd774f2de1e0f94cdee068457bb8e73640cc4
starlightroad <ortizge92@gmail.com>
2026-05-12 23:54:58 -0400

    Change new device cancel button to a link

--- src/app/(dashboard)/devices/new/page.tsx
+++ src/app/(dashboard)/devices/new/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { getDeviceTypes } from "@/dal/type";
 
 import { getDeviceGroups } from "@/dal/group";
@@ -6,9 +8,11 @@ import { getDeviceStatuses } from "@/dal/status";
 
 import { FORM_ID } from "@/features/device/lib/constants";
 
+import { DEVICES_PATH } from "@/features/dashboard/lib/constants";
+
 import { createSelectFieldItems } from "@/features/device/lib/helpers";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 
 import NewDeviceForm from "@/features/device/components/new-device-form";
 
@@ -38,9 +42,9 @@ export default async function NewDevicePage() {
             />
           </CardContent>
           <CardFooter className="justify-end gap-2">
-            <Button type="button" variant="outline">
+            <Link href={DEVICES_PATH} className={buttonVariants({ variant: "outline" })}>
               Cancel
-            </Button>
+            </Link>
             <Button form={FORM_ID} type="submit">
               Create Device
             </Button>
