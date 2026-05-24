c4734efdf8078ed3fc85bb3c3a027ec0a6c71061
starlightroad <ortizge92@gmail.com>
2026-05-23 21:22:37 -0400

    Update download button component with new hook

--- src/features/profile/components/download-button.tsx
+++ src/features/profile/components/download-button.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
-import { DOWNLOAD_CSV_URL } from "@/features/device/lib/constants";
-
 import { Button } from "@/components/ui/button";
 
-export default function DownloadButton() {
-  const { push } = useRouter();
+import useDownloadDevices from "@/features/device/hooks/use-download-devices";
 
-  const handleDownload = async () => {
-    push(DOWNLOAD_CSV_URL);
-  };
+export default function DownloadButton() {
+  const { handleDownload } = useDownloadDevices();
 
   return (
     <Button type="button" variant="outline" aria-label="Download data as CSV" onClick={handleDownload}>
